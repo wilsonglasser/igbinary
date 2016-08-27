d4fabc0cf6af222f55fcf2372ec464c559c239a7
Tyson Andre <tyson.andre@uwaterloo.ca>
2016-08-26 19:51:38 -0700

    Fixes #72: Add a php_igbinary.h file to allow statically compiling php (#73)
    I can't think of any reasons for php_igbinary.h to be used by other
    extensions, so not checking for pecl directory.

--- php_igbinary.h
+++ php_igbinary.h
@@ -1,8 +1,8 @@
 #include "php_version.h"
 #if PHP_MAJOR_VERSION == 5
-#include "src/php5/igbinary.h"
+#include "ext/igbinary/src/php5/php_igbinary.h"
 #elif PHP_MAJOR_VERSION == 7
-#include "src/php7/igbinary.h"
+#include "ext/igbinary/src/php7/php_igbinary.h"
 #else
 #error "Unsupported php version for igbinary build"
 #endif
