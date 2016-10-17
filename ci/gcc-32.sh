00d8ea218325c44adec1175a6614d1901653a06f
Tyson Andre <tyson.andre@uwaterloo.ca>
2016-10-16 17:53:02 -0700

    Run tests with 32-bit php on travis. (#67)
    The serializer contains 32-bit specific code and code dependent on
    pointer lengths, and this should be tested.
    
    `gcc -m32 ...` (ci/gcc-32.sh) builds 32-bit versions of binaries, including php.
    (It will test the case where SIZEOF_ZEND_LONG == 4)
    The 64-bit travis machines are capable of running those binaries.
    
    This takes 1 minute to compile, so a travis caching step was added.
    This has multiple layers of cache, trying to first specify the most
    specific cache, then a more generic one for the whole project.
    (The cache may have a different php installation, which is given a
    different name so it can be detected and removed)
    
    Based on https://github.com/runkit7/runkit7/blob/master/.travis.yml in
    another project I work on.
    
    Install CC before replacing it with a shell script for 32-bit
    Add the flag --enable-json for php >= 7.0.10

---
+++ ci/gcc-32.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec gcc-4.6 -m32 "$@"
---
+++ ci/install_php_custom.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xeu
+
+echo "Attempting to install PHP from source, PHP version '$PHP_CUSTOM_VERSION'/ configure args '$PHP_CONFIGURE_ARGS'"
+if [ "x$PHP_CUSTOM_VERSION" = "x" -o "x$PHP_CONFIGURE_ARGS" = "x" ] ; then 
+	echo "Missing php version or configuration arguments";
+	exit 1;
+fi
+PHP_FOLDER="php-$PHP_CUSTOM_VERSION"
+PHP_INSTALL_DIR="$(./ci/generate_php_install_dir.sh)"
+echo "Downloading $PHP_INSTALL_DIR\n"
+if [ -x $PHP_INSTALL_DIR/bin/php ] ; then 
+	echo "PHP $PHP_CUSTOM_VERSION already installed and in cache at $PHP_INSTALL_DIR";
+	exit 0
+fi
+# Remove cache if it somehow exists
+if [ "x${TRAVIS:-0}" != "x" ]; then
+	rm -rf $HOME/travis_cache/
+fi
+# Otherwise, put a minimal installation inside of the cache.
+PHP_TAR_FILE="$PHP_FOLDER.tar.bz2"
+curl --verbose https://secure.php.net/distributions/$PHP_TAR_FILE -o $PHP_TAR_FILE
+# If we decide to support 7.1.0, it would be `curl --verbose https://downloads.php.net/~davey/php-7.1.0RC3.tar.bz2 -o $PHP_TAR_FILE`
+tar xjf $PHP_TAR_FILE
+
+pushd $PHP_FOLDER
+./configure $PHP_CONFIGURE_ARGS --prefix="$PHP_INSTALL_DIR"
+make -j5
+make install
+popd
+
+echo "PHP $PHP_CUSTOM_VERSION successfully installed and in cache at $HOME/travis_cache/$PHP_FOLDER";
