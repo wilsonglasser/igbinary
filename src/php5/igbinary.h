68ea3eca2ef392de974c521ea64105fd8480b4c0
Tyson Andre <tyson.andre@uwaterloo.ca>
2016-10-30 16:14:55 -0700

    Update NEWS file, IGBINARY_VERSION in header files (#93)
    For Issue #86

--- src/php5/igbinary.h
+++ src/php5/igbinary.h
@@ -22,7 +22,7 @@ struct zval;
 /** Binary protocol version of igbinary. */
 #define IGBINARY_FORMAT_VERSION 0x00000002
 
-#define PHP_IGBINARY_VERSION "1.2.2-dev"
+#define PHP_IGBINARY_VERSION "1.3.0"
 
 /* Macros */
 #ifdef PHP_WIN32
