d71892ae0692290e78788f16946a53dbc1d10c0c
Tyson Andre <tysonandre775@hotmail.com>
2019-12-08 17:21:56 -0500

    Fix downloading closest php tarball for 7.4-snapshot versions

--- ci/get_global_php_version.sh
+++ ci/get_global_php_version.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-php -r "echo(preg_replace('/-dev|RC[0-9]+$/','',PHP_VERSION));"
+php -r "if (preg_match('/-dev|RC[0-9]+/', PHP_VERSION)) { printf('%d.%d.%d', PHP_MAJOR_VERSION, PHP_MINOR_VERSION, max(PHP_RELEASE_VERSION-1,0)); } else { echo PHP_VERSION; }"
