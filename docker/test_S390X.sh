f34dc11af1ac7e3fa5422d0a02f044bdfbe65d9a
Tyson Andre <tysonandre775@hotmail.com>
2022-10-11 20:14:43 -0400

    Add WIP scripts for testing locally
    This uses docker's multi-arch support

---
+++ docker/test_S390X.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+IMAGE_NAME=pecl-igbinary-s390x-emulated-8.0.13
+docker build --platform S390X --tag=$IMAGE_NAME --build-arg=PHP_VERSION=8.0.13 -f docker/Dockerfile . || exit 1
+exec docker run --rm --platform S390X $IMAGE_NAME /bin/bash -c 'export CFLAGS=-O1; make test TESTS="--show-diff -j$(nproc)"'
